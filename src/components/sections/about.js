fbd82d8c92287aa9af008298c70c071b6a8b37f0
Humza A A <127075542+HumzAhme@users.noreply.github.com>
2025-02-13 11:58:25 +0000

    intro

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -52,7 +52,7 @@ const StyledPic = styled.div`
 
   @media (max-width: 768px) {
     margin: 50px auto 0;
-    width: 70%;
+    width: 40%;
   }
 
   .wrapper {
@@ -125,7 +125,14 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = [
+    'JavaScript (ES6+)',
+    'Python Libraries',
+    'Python',
+    'Linux Terminal',
+    'Node.js',
+    'Transformers',
+  ];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,30 +142,19 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
-            </p>
-
-            <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              Hello! My name is HUMZA, a dynamic and unconventional thinker, dedicated to pushing
+              boundaries and exploring innovative ideas. I have done{' '}
+              <a href="https://starry.com/">considerable projects </a>, and{' '}
+              <a href="https://www.apple.com/"> certifications</a>, in secure development & blue
+              team cybersecurity. My dedication to completing projects thoroughly, often working
+              tirelessly until they're finished, sets me apart. My rational thinking and analytical
+              skills of approaching any problem, honed through critical research, have earned me
+              recognition for my ability to devise solutions to incurring problems at-hand, making
+              me adept at both qualitative and quantitative tasks.{' '}
+              <a href="https://starry.com/">My research</a>, in cybersecurity, AI, and their
+              potential impacts on societal aspects is my passion. Other than academics and career,
+              i am actively engaged in sports activities for whom I have done some{' '}
+              <a href="https://starry.com/"> volunteer work </a>, too.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
