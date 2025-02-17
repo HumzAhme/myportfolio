2e1be5e7526d673bafb2351385532dd50ca3b787
Humza A A <127075542+HumzAhme@users.noreply.github.com>
2025-02-17 17:17:03 +0000

    logo+colors

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,4 +2,20 @@
  * Implement Gatsby's Browser APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+ */ // yarn add cursor-effects
+// export const onInitialClientRender = () => {
+//     const script = document.createElement('script');
+//     script.src = "https://unpkg.com/cursor-effects/dist/cursor-effects.min.js";
+//     script.async = true;
+//     document.body.appendChild(script);
+
+//     script.onload = () => {
+//       // Now the cursor classes are available on window
+//       new window.RibbonCursor({
+//         colorSaturation: '80%',
+//         colorBrightness: '60%',
+//         colorAlpha: 0.5,
+//         strokeSize: 5,
+//       });
+//     };
+//   };
--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -1,20 +1,29 @@
 import React from 'react';
 
 const IconLogo = () => (
-  <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
+  <svg
+    id="logo"
+    xmlns="http://www.w3.org/2000/svg"
+    role="img"
+    viewBox="0 0 84 96"
+    width="84"
+    height="96">
     <title>Logo</title>
-    <g transform="translate(-8.000000, -2.000000)">
-      <g transform="translate(11.000000, 5.000000)">
-        <polygon
-          id="Shape"
-          stroke="currentColor"
-          strokeWidth="5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          points="39 0 0 22 0 67 39 90 78 68 78 23"
-        />
+    {/* Shift everything slightly down-right to avoid clipping */}
+    <g transform="translate(3,3)">
+      {/* The hexagon shape */}
+      <polygon
+        stroke="currentColor"
+        strokeWidth="5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        fill="none"
+        points="39 0 0 22 0 67 39 90 78 68 78 23"
+      />
+      {/* Position & scale the 'H' path so it sits near the center */}
+      <g transform="translate(14,17) scale(4)">
         <path
-          d="M45.691667,45.15 C48.591667,46.1 50.691667,48.95 50.691667,52.2 C50.691667,57.95 46.691667,61 40.291667,61 L28.541667,61 L28.541667,30.3 L39.291667,30.3 C45.691667,30.3 49.691667,33.15 49.691667,38.65 C49.691667,41.95 47.941667,44.35 45.691667,45.15 Z M33.591667,43.2 L39.241667,43.2 C42.791667,43.2 44.691667,41.85 44.691667,38.95 C44.691667,36.05 42.791667,34.8 39.241667,34.8 L33.591667,34.8 L33.591667,43.2 Z M33.591667,47.5 L33.591667,56.5 L40.191667,56.5 C43.691667,56.5 45.591667,54.75 45.591667,52 C45.591667,49.2 43.691667,47.5 40.191667,47.5 L33.591667,47.5 Z"
+          d="M1.88494 12V1.81818H4.03764V6.01918H8.40767V1.81818H10.5554V12H8.40767V7.79403H4.03764V12H1.88494Z"
           fill="currentColor"
         />
       </g>
