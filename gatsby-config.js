e7240f34dfd785ba9299da79f2c86fcfb224891d
Humza A A <127075542+HumzAhme@users.noreply.github.com>
2025-02-26 19:22:01 +0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 const config = require('./src/config');
 
 module.exports = {
-  pathPrefix: "/myPortfolio", // other configuration
+  pathPrefix: "/myportfolio", // other configuration
   siteMetadata: {
     title: 'Humza Asrar A',
     description:
